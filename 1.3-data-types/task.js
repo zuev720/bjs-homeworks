028e6224e16d7dbcc23b2b18f61358f83e113616
zuev720 <zuev720mail.ru>
2020-11-04 12:23:41 +0300

    1.3-data-types

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,26 @@
+"use strict";
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
     // код для задачи №1 писать здесь
-    // return totalAmount;
+    let totalAmount;
+    let monthlyPayment;
+    let now = new Date();
+    let numberPaidMonths = Math.round(((date - now) / ((1000 * 60 * 60 * 24 * 365) / 12)));
+    if (isNaN(percent && contribution && amount) || (date == "Invalid Date")) {
+        totalAmount = "Введены неверные данные";
+    } else {
+    let bodyCredit = amount - contribution;
+    percent = percent / 100 / 12;
+    monthlyPayment =  bodyCredit * percent / (1 - Math.pow(1 + percent, -numberPaidMonths));
+    totalAmount = monthlyPayment * numberPaidMonths;
+    totalAmount = Number(totalAmount.toFixed(2));
+    }
+        return totalAmount;
 }
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
-    // return greeting;
+    name = (name === "" || name === null || name === undefined) ? "Аноним": name;
+    let greeting = `Привет, мир! Меня зовут ${name}`;
+    return greeting;
 }
