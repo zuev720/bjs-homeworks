33d5c1ccada8ef6862f623ddf100d7f1a85b7c2d
zuev720 <zuev720mail.ru>
2020-11-28 18:58:10 +0300

    3.2-array-proto-hocs first fixed

--- 3.2-array-proto-hocs/functions.js
+++ 3.2-array-proto-hocs/functions.js
@@ -24,18 +24,16 @@ function getTotalDamage() {
    return  weapons.map((weapon) => weapon.attack).reduce((sum, current) => sum + current);
 }
 
-function getValuesCountToSumValues(arr, sumNumbers){
-    let sum = 0;
-    let length = 0;
-
-    arr.map(function(elem){
-        if(sum < sumNumbers){
-            sum += elem;
-            length++;
+function getValuesCountToSumValues(arr, total) {
+    let count = 1;
+    arr.reduce((sum, element) => {
+        sum += element;
+        if (sum < total) {
+            count++;
         }
-    });
-
-    return length;
+        return sum;
+    }, 0)
+    return count;
 }
 
 // Задание 2
@@ -60,32 +58,35 @@ function compareArrays(arr1, arr2) {
 function memorize(fn, limit) {
     let memory = [];
     return (...args) => {
-        const result = memory.find(answer => compareArrays(answer.args, args));
+        let result = memory.find(answer => compareArrays(answer.args, args));
         if (result) {
             console.log("Достаю из памяти")
             return result.result;
         }
+        else {
+            result = fn(...args);
+            memory.push({
+                args: args,
+                result: result
+            });
 
-        memory.push({
-            args: args,
-            result: fn(...args)
-        });
+            if (memory.length > limit) {
+                console.log("Удаляю")
+                memory.shift()
+            }
 
-        if (memory.length > limit) {
-            console.log("Удаляю")
-            memory.shift()
+            console.log("Вычисляю")
+            return result;
         }
-
-        console.log("Вычисляю")
-        return fn(...args);
     }
 }
 
-console.time("timer");
 
 const tasteArray = [ [1,2,3], [1,2], [1,2,3], [1,2], [9,5,2,4] ];
 
 function testCase(testFunction, someTimer) {
+console.time("timer");
+
     for (let i = 0; i < 100; i++){
         tasteArray.forEach(element => testFunction(...element));
     };
@@ -94,5 +95,5 @@ function testCase(testFunction, someTimer) {
 
 testCase(sum, "timer");
 
-// timer: 499.4228515625 ms  с замедлением времени
-// timer: 0.259765625 ms без замедления
+// timer: 499.6240234375 ms  с замедлением времени
+// timer: 0.27099609375 ms без замедления
